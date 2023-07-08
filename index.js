b81f05ce32b1731e59a3b67fd362e4de81bd6d1f
Harsh Singh <singhharsh8283@gmail.com>
2023-07-08 10:11:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const sizes = document.querySelector(".sizes");
 
 
 
-
 dark.addEventListener("input", handleDarkColor);
 light.addEventListener("input", handleLightColor);
 qrText.addEventListener("input", handleQRText);
